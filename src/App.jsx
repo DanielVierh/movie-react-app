0b0db4a61d394580c40224e74bcdc844ef89782d
DanielVierh <daniel.vierheilig89@yahoo.de>
2025-06-17 10:46:09 +0200

    add assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h2>Hello World</h2>
-    </div>
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="./hero.png" alt="Hero Banner"/>
+            <h1>Find the <span className="text-gradient">Movies</span> You'll Enjoy Without the Hussle</h1>
+          </header>
+          <p>Search</p>
+        </div>
+      </div>
+    </main>
   )
 }
 
