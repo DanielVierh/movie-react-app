20e1e6f66d81f2af2cd7c28eca123130662aa66d
DanielVierh <daniel.vierheilig89@yahoo.de>
2025-07-25 20:47:03 +0200

    change btn styles

--- src/components/StickyHeader.jsx
+++ src/components/StickyHeader.jsx
@@ -5,7 +5,7 @@ const StickyHeader = ({title, link_to_main}) => {
   return (
     <div>
         <header className='sticky-header'>
-              <a href={link_to_main} className='text-start px-10 py-5 text-2xl fit-content cursor-pointer bg-blue-950 w-fit'>Home</a>
+              <a href={link_to_main} className='type-cta text-start px-7 py-3 mt-10 mb-5 text-xl fit-content cursor-pointer w-fit'>Home</a>
              <h2 className='text-center pb-5'>{title}</h2>
         </header>
     </div>
