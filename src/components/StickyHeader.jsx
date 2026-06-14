7d8b2479da71ab17b30a741c6b12c0beb0a2f9e4
DanielVierh <daniel.vierheilig89@yahoo.de>
2026-06-14 23:05:11 +0200

    remove home btn

--- src/components/StickyHeader.jsx
+++ src/components/StickyHeader.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import GoBackButton from './GoBackButton'
+import React from "react";
+import GoBackButton from "./GoBackButton";
 
-const StickyHeader = ({title, link_to_main}) => {
+const StickyHeader = ({ title }) => {
   return (
     <div>
-        <header className='sticky-header'>
-              <a href={link_to_main} className='type-cta text-start px-7 py-3 mt-10 mb-5 text-xl fit-content cursor-pointer w-fit'>Home</a>
-             <h2 className='text-center pb-5'>{title}</h2>
-        </header>
+      <header className="sticky-header">
+        <h2 className="text-center pb-5">{title}</h2>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default StickyHeader
+export default StickyHeader;
